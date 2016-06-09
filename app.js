e02acdee29d5e123c90f959e5f6e664b43976d51
doloreel <jarimado@gmail.com>
2016-06-09 21:43:52 +1000

    changing computer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express   = require('express');
 const app       = express();
 const path      = require("path");
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3005);
 
 app.use(express.static(__dirname + '/public'));
 
--- src/index.js
+++ src/index.js
@@ -2,24 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-
-const Bottle = () => {
-    return (
-        <div id="bottle">
-            <ul>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga saepe, blanditiis earum id inventore odit molestiae maxime minima molestias itaque eaque, tempora quos accusantium eligendi animi quisquam harum eius laudantium.
-                </li>
-                <li>Magnam, quam, quaerat qui ratione dignissimos officiis alias optio sapiente illo dicta voluptas unde dolor autem, ullam suscipit consectetur sit eum dolorum officia. Quae voluptatum, natus vitae iste delectus tempore?
-                </li>
-                <li>Quae atque cum soluta ea facilis aliquid aperiam odit a delectus pariatur eum ut nihil earum animi quis quia possimus nisi culpa, ad unde similique officiis harum illo deleniti ratione!
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-
-
 class App extends React.Component {
     constructor() {
         super();
@@ -30,12 +12,9 @@ class App extends React.Component {
             <div>
                 <div id="gameWrapper">
                 </div>
-                <Bottle />
             </div>
         )
     }
 }
-
-
 
 ReactDOM.render(<App/> , document.getElementById('master'))
